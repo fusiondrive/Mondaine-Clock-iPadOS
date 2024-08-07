d7e5972fa55e0b0de54388f70c668b68cf963789
WoodenAxe <473072010@qq.com>
2024-08-08 02:30:31 +0800

    Initial Commit

---
+++ Mondaine Clock/Mondaine_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mondaine_ClockApp.swift
+//  Mondaine Clock
+//
+//  Created by Steve Wang on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Mondaine_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mondaine ClockTests/Mondaine_ClockTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mondaine_ClockTests.swift
+//  Mondaine ClockTests
+//
+//  Created by Steve Wang on 8/8/24.
+//
+
+import Testing
+
+struct Mondaine_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
